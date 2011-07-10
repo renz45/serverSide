7398cac0e700c32bae45ff8e04091ebd07ee816c
Adam Rensel <adamrensel@gmail.com>
2011-07-10 12:46:41 -0400

    added javascript to add functionality to the options page. Added ajax methods to provide creation, pinning, editing, deleting of lists
    Signed-off-by: Adam Rensel <adamrensel@gmail.com>

--- cil/js/cil_optionMenu.js
+++ cil/js/cil_optionMenu.js
@@ -1,3 +1,188 @@
-jQuery(document).ready(function($) {
-   $('#test').click(function(){alert("TEST");});
+jQuery(document).ready(function() {
+
+	////////////////////////////////////////////////////////////
+	//														  //
+	//				Page functionality + ajax				  //
+	//														  //
+	////////////////////////////////////////////////////////////
+
+	////////expand the form when the add new list button is pressed/////////
+	jQuery("#create_new_list_header").bind('click',function(){
+		var form = jQuery("#cil_edit_List_form");
+
+		if(form.css('height') == "0px")
+		{
+			form.animate({height:'480px'},350).css({height:'auto'});
+		}
+
+		//clear form
+		clearForm();
+
+		//change submit button label
+		changeSubmitLabel("Create a New List");
+
+	});
+
+	///////////list edit button functionality////////////
+	jQuery(".cil_list_edit_btn").bind('click',editButtonHandler);
+
+	function editButtonHandler(){
+		var name = jQuery(this).parent().find(".cil_list_name span").html();
+
+		//change submit button label
+		changeSubmitLabel("Edit " + name + " List");
+
+		//expand the edit menu if it's closed
+		var form = jQuery("#cil_edit_List_form");
+		if(form.css('height') == "0px")
+		{
+			form.animate({height:'480'},350).css({height:'auto'});
+		}
+
+		//take values from the list of item lists and insert them into the form for editing
+		jQuery("#cil_listName").attr("value", name);
+		jQuery("#cil_listDescription").attr("value", jQuery(this).parent().find('.desc').html());
+		jQuery("#cil_iconUrl").attr("value", jQuery(this).parent().find('.cil_icon_url').attr('src'));
+		jQuery("#cil_logoUrl").attr("value", jQuery(this).parent().find('.cil_logo_url').attr('src'));
+		jQuery("#cil_edit_List_form").attr('action', jQuery(this).parent().attr('id').split("cil-list_")[1]);
+	}
+
+	////////////Ajax pin List////////////////
+	jQuery(".cil_pin_btn").bind('click',pinButtonHandler);
+	//toggle pinned state
+	function pinButtonHandler(){
+		var pinned = '1';
+		if(jQuery(this).hasClass('active'))//if the item has the class active which means it is pinned
+		{
+			pinned = '0';
+		}else{//no active class and is not pinned
+			pinned = '1';
+			jQuery(this).addClass('active');
+		}
+
+		var data = {
+				action:'cil_pin_list',
+				isPinned: pinned,
+				id: jQuery(this).parent().attr('id').split("cil-list_")[1]
+		};
+
+		jQuery.post(ajaxurl, data,function(r){
+			var obj = jQuery.parseJSON(r);
+
+			if(obj['isPinned'] == 0)
+			{
+				jQuery('#cil-list_'+obj['id']+" .cil_pin_btn").removeClass('active');
+			}else{
+				jQuery('#cil-list_'+obj['id']+" .cil_pin_btn").addClass('active');
+			}
+		});
+	}
+
+	//////////////Ajax delete list//////////////
+	jQuery(".cil_list_delete_btn").bind('click',deleteButtonHandler);
+	//delete an item
+	function deleteButtonHandler(){
+
+		var data = {
+				action:'cil_delete_list',
+				id: jQuery(this).parent().attr('id').split("cil-list_")[1]
+		};
+
+		//delete list from the database
+		jQuery.post(ajaxurl, data,function(r){
+			//remove the item from the list
+			jQuery("#cil-list_"+r).remove();
+		});
+	}
+
+	//////////////Ajax edit list////////////////
+	jQuery("#cil_edit_List_form").bind('submit',function(event){
+		//prevent the default form submit action
+		event.preventDefault();
+
+		//create data object for ajax call
+		var data = {
+				action:'cil_edit_list',
+				id: jQuery(this).attr('action'),
+				listName: jQuery("#cil_listName").attr("value"),
+				logoUrl: jQuery("#cil_logoUrl").attr("value"),
+				iconUrl: jQuery("#cil_iconUrl").attr("value"),
+				listDesc: jQuery("#cil_listDescription").attr("value")
+		};
+		//update list
+
+		//perform the ajax call
+		jQuery.post(ajaxurl, data,function(r){
+			//if the upload is successful
+
+			//parse the return JSON object into a javascript object
+			var data = jQuery.parseJSON(r);
+
+			if(data['newPost'] == true)
+			{//if the ajax call was a new post than create a new list item for the list, use the returned data object to populate values
+				jQuery('.cil_admin_list').append(
+				"<li id='cil-list_"+ data['id'] +"'>"+
+				"<a class='cil_pin_btn cil_list_btn' title='Pin this list to menu'>pin</a>"+
+				"<img class='cil_icon_url' src='"+data['iconUrl']+"' alt='"+ data['listName'] +"-"+data['iconUrl']+"' title='"+data['iconUrl']+"' width='20' height='20' />"+
+				"<img class='cil_logo_url' src='"+data['logoUrl']+"' alt='"+ data['listName'] +"-"+data['logoUrl']+"' title='"+data['logoUrl']+"' width='0' height='0' />"+
+				"<span class='cil_list_name'> - <span>"+ data['listName'] +"</span> - </span>"+
+				"<p class='desc'>"+data['listDesc']+"</p>"+
+				"<a class='cil_list_btn cil_list_edit_btn'>Edit</a>"+
+				"<a class='cil_list_btn cil_list_delete_btn'>Delete</a>"+
+				"</li>\n");
+
+				//rebind click handlers to the buttons in the new list item, this is done automatically on page reload, but this is for when a list item is
+				//initally created so the page doesn't have to reload in order to get functionality
+				var newItem = jQuery('#cil-list_'+ data['id']);
+
+				newItem.find('.cil_list_edit_btn').bind('click',editButtonHandler);
+				newItem.find('.cil_pin_btn').bind('click',pinButtonHandler);
+				newItem.find('.cil_list_delete_btn').bind('click',deleteButtonHandler);
+
+			}else{//if the list item exists and it's just being edited, change the values in the main list to match the edits
+				var list = jQuery("#cil-list_" + data['id']);
+
+				var logo = list.find('.cil_logo_url');
+				var icon = list.find('.cil_icon_url');
+
+				logo.attr('alt', data['listName']+"-"+data['logoUrl']);
+				icon.attr('alt', data['listName']+"-"+data['iconUrl']);
+				list.find('.cil_list_name span').html(data['listName']);
+				logo.attr('src', data['logoUrl']);
+				logo.attr('title',data['logoUrl']);
+				icon.attr('src', data['iconUrl']);
+				icon.attr('title',data['iconUrl']);
+				list.find('.desc').html(data['listDesc']);
+			}
+			//close the form
+			jQuery("#cil_edit_List_form").animate({height:'0px'},350).css({height:'auto'});
+
+			//clear form
+			clearForm();
+
+			//change submit button label back to the default value
+			changeSubmitLabel("Create a New List");
+
+		});
+	});
+
+	////////////clear edit list form//////////////
+	function clearForm(){
+		jQuery("#cil_listName").attr("value", "");
+		jQuery("#cil_listDescription").attr("value", "");
+		jQuery("#cil_iconUrl").attr("value", "");
+		jQuery("#cil_logoUrl").attr("value", "");
+		jQuery("#cil_edit_List_form").attr('action', "");
+	}
+
+	//////////////change submit button label///////////////
+	function changeSubmitLabel(label){
+		jQuery('#cil_newListSubmit').attr('value', label);
+	}
+
+	////////////////////////////////////////////////////////////
+	//														  //
+	//					Form Validation						  //
+	//														  //
+	////////////////////////////////////////////////////////////
 });
