da41abe2fca68613a63b0584681a32a60362b557
Adam Rensel <adamrensel@gmail.com>
2011-07-12 17:03:39 -0400

    changed list item table to include isHidden
    Signed-off-by: Adam Rensel <adamrensel@gmail.com>

--- cil/models/cil_model_installDataBase.php
+++ cil/models/cil_model_installDataBase.php
@@ -50,12 +50,13 @@ function cil_install () {
 
 		$sql = "CREATE TABLE " . $table_name . " (
 			  id mediumint(9) NOT NULL AUTO_INCREMENT,
-			  heading VARCHAR(128) NOT NULL,
+			  heading VARCHAR(200) NOT NULL,
 			  time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
 			  description text NOT NULL,
 			  url VARCHAR(55) DEFAULT '' NOT NULL,
 			  image_url VARCHAR(55) DEFAULT '' NOT NULL,
 			  list_id int(55) DEFAULT 0 NOT NULL,
+			  isHidden tinyint(1) DEFAULT '0' NOT NULL,
 			  UNIQUE KEY id (id)
 			);";
 
