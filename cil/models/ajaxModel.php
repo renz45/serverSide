eb0a448a74abdabbf36f0a056fa09312ab1fdcac
Adam Rensel <adamrensel@gmail.com>
2011-07-12 22:04:42 -0400

    added edit list item ajax model
    Signed-off-by: Adam Rensel <adamrensel@gmail.com>

--- cil/models/ajaxModel.php
+++ cil/models/ajaxModel.php
@@ -44,7 +44,7 @@ function cil_delete_list() {
 }
 
 add_action('wp_ajax_cil_edit_list', 'cil_edit_list');
-//delete a list
+//edit a list
 function cil_edit_list() {
 	global $wpdb;
 
@@ -53,14 +53,15 @@ function cil_edit_list() {
 	$id = "";
 	$newPost = false;
 
+	//if an id was given as a post variable return just that list
 	if(!empty($_POST['id'] ))
 	{
 		$id = $_POST['id'];
 		$sql = "UPDATE " . $table_name . " SET name='". $_POST['listName'] ."', time=NOW(), description='". $_POST['listDesc'] ."', logo_url='". $_POST['logoUrl'] ."', icon_url='". $_POST['iconUrl'] ."' WHERE id='". $id ."'";
 
-		$wpdb->prepare($sql);
+		$wpdb->prepare($sql);//security measures, stripping slashes and such with wp built in prepare method
 		$wpdb->query($sql);
-	}else{
+	}else{//if there was no id return all lists
 		$name = $_POST['listName'];
 		$description = $_POST['listDesc'];
 		$logo_url = $_POST['logoUrl'];
@@ -106,11 +107,11 @@ function cil_hide_list() {
 }
 
 add_action('wp_ajax_cil_delete_listItem', 'cil_delete_listItem');
-//delete a list
+//delete a list item
 function cil_delete_listItem() {
 	global $wpdb;
 
-	//delete list with specified id
+	//delete list item with specified id
 	$tableName = $wpdb->prefix . "cil_listItemInfo";
 	$sql = "DELETE FROM ". $tableName ." WHERE id='". $_POST['id'] ."'";
 
@@ -119,5 +120,55 @@ function cil_delete_listItem() {
 
    	echo $_POST['id'];
 
+	die(); // this is required to return a proper result
+}
+
+add_action('wp_ajax_cil_edit_item', 'cil_edit_item');
+//edit a list item
+function cil_edit_item() {
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . "cil_listItemInfo";
+
+	$id = "";
+	$newItem = false;
+
+	//if an id was given as a post variable return just that list
+	if(!empty($_POST['id'] ))
+	{
+		$id = $_POST['id'];
+		$sql = "UPDATE " . $table_name . "
+				SET
+					heading='". $_POST['heading'] ."',
+				    time=NOW(),
+				    content='". $_POST['content'] ."',
+				    image_url='". $_POST['imageUrl'] ."',
+				    url='". $_POST['url'] ."',
+				    list_id='". $_POST['listId'] ."'
+				WHERE id='". $id ."'";
+
+		$wpdb->prepare($sql);//security measures, stripping slashes and such with wp built in prepare method
+		$wpdb->query($sql);
+	}else{//if there was no id return all lists
+		$heading = $_POST['heading'];
+		$content = $_POST['content'];
+		$image_url = $_POST['imageUrl'];
+		$url = $_POST['url'];
+		$list_id = $_POST['listId'];
+
+		$wpdb->insert($table_name, array('heading' => $heading,'time' => current_time('mysql'),'content' => $content,'image_url' => $image_url,'url' => $url, 'list_id' => $list_id ));
+		$id = $wpdb->insert_id;//get recently created id
+		$newItem = true;
+	}
+
+	$return = json_encode(array('id'=>$id,
+								'heading'=>$_POST['heading'],
+								'content'=>$_POST['content'],
+								'imageUrl'=>$_POST['imageUrl'],
+								'url'=>$_POST['url'],
+								'listId'=>$_POST['listId'],
+								'newItem'=>$newItem));
+
+	echo $return;
 	die(); // this is required to return a proper result
 }
