54b5779ec3925c3c69bb5b0311a955557fbda5ec
Adam Rensel <adamrensel@gmail.com>
2011-07-22 22:26:24 -0400

    added a drag to re-order line to notify people of the drag and drop re-ordering
    Signed-off-by: Adam Rensel <adamrensel@gmail.com>

--- cil/views/cil_view_help.php
+++ cil/views/cil_view_help.php
@@ -46,18 +46,19 @@ class cil_view_contextualHelp {
 		$pageName = $pageName[0];
 
 		$ret = "<div id='cil_edit_item_help'>
-								<p>This list can be displayed in a post or page copying and pasting following short code into the text area: <code>[cil name=\"$pageName\"]</code></p>
+					<p>This list can be displayed in a post or page copying and pasting following short code into the text area: <code>[cil name=\"$pageName\"]</code></p>
 
-								<p>The tag above is the basic tag that is required, there are optional attributes which can be used, which are:</p>
-								<ul>
-									<li>'<code>name=\"$pageName\"</code>' - name of the list you want to display - this attribute is required</li>
-									<li> '<code>use_item_url=\"true\"</code>' - This defaults to 'true', setting this to false will force the item headings to never be links, even if a url is given</li>
-									<li>'<code>show_time=\"false\"</code>' - Shows the time that the item was created, this is set to 'false' by default</li>
-									<li>'<code>image_width=\" \"</code>' - Force the images for each list item to be this width (this should be set if all images are one size, sets the height and width in the &lt;img&gt; tag)</li>
-									<li>'<code>image_height=\" \"</code>' - Force the images for each list item to be this height (this should be set if all images are one size, sets the height and width in the &lt;img&gt; tag)</li>
-									<li>'<code>link_target=\"_blank\"</code>' - Setting this will determine how links open, accepts set to _blank by default: _blank(new window) _top(top frame) _parent(current frame) _self(current window)</li>
-								</ul>
-							</div>";
+					<p>The tag above is the basic tag that is required, there are optional attributes which can be used, which are:</p>
+					<ul>
+						<li>'<code>name=\"$pageName\"</code>' - name of the list you want to display - this attribute is required</li>
+						<li> '<code>use_item_url=\"true\"</code>' - This defaults to 'true', setting this to false will force the item headings to never be links, even if a url is given</li>
+						<li>'<code>show_time=\"false\"</code>' - Shows the time that the item was created, this is set to 'false' by default</li>
+						<li>'<code>image_width=\" \"</code>' - Force the images for each list item to be this width (this should be set if all images are one size, sets the height and width in the &lt;img&gt; tag)</li>
+						<li>'<code>image_height=\" \"</code>' - Force the images for each list item to be this height (this should be set if all images are one size, sets the height and width in the &lt;img&gt; tag)</li>
+						<li>'<code>link_target=\"_blank\"</code>' - Setting this will determine how links open, accepts set to _blank by default: _blank(new window) _top(top frame) _parent(current frame) _self(current window)</li>
+					</ul>
+					<p>Items can be re-organized by dragging and dropping</p>
+				</div>";
 		return $ret;
     }
 
