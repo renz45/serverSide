768dd640af89ceb7312e35c8e7f35628e7c0af02
Adam Rensel <adamrensel@gmail.com>
2011-07-30 20:28:11 -0400

    added drop table
    Signed-off-by: Adam Rensel <adamrensel@gmail.com>

--- cil/models/cil_model_installDataBase.php
+++ cil/models/cil_model_installDataBase.php
@@ -1,37 +1,11 @@
 <?php
 
-/*
-cil_listInfo:
----------------------
-	id
-	time
-	name
-	description
-	isPinned (id of list to be pinned under. 0 indicates a new main menu item)
-	logo_url
-	icon_url
-	template
-
-cil_listItemInfo:
----------------------
-	id
-	time
-	heading
-	content
-	url
-	image_url
-	list_id
-	isHidden
-	index
-*/
-
 global $cil_db_version;
-$cil_db_version = "1.0";
-
-add_action('plugins_loaded', 'myplugin_update_db_check');
 
 function cil_install () {
    global $wpdb;
+
+   add_option('cil_uninstall_db', false);
 
    $table_name = $wpdb->prefix . "cil_listInfo";
 
@@ -71,48 +45,25 @@ function cil_install () {
 
 	dbDelta($sql);
 
-	//global $cil_db_version;
-	//add_option("cil_db_version", $cil_db_version);
-	/* // update database on version change example code
-	$installed_ver = get_option( "cil_db_version" );
-
-	   if( $installed_ver != $cil_db_version ) {
-
-	      $sql = "CREATE TABLE " . $table_name . " (
-		  id mediumint(9) NOT NULL AUTO_INCREMENT,
-		  time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-		  name tinytext NOT NULL,
-		  text text NOT NULL,
-		  url VARCHAR(100) DEFAULT '' NOT NULL,
-		  UNIQUE KEY id (id)
-		);";
-
-	      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-	      dbDelta($sql);
-
-	      update_option( "jal_db_version", $jal_db_version );
-	  }
-	  */
 }
-/*
-$table_name = $wpdb->prefix . "cil_listInfo";
+//if the option to delete the tables on uninstall is checked then drop the tables used for cil
+function cil_uninstall()
+{
 
-	$sql = "CREATE TABLE " . $table_name . " (
-		  id mediumint(9) NOT NULL AUTO_INCREMENT,
-		  time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-		  title VARCHAR(128) NOT NULL,
-		  description text NOT NULL,
-		  url VARCHAR(55) DEFAULT '' NOT NULL,
-		  icon_url VARCHAR(55) DEFAULT '' NOT NULL,
-		  pin_id int(55) DEFAULT 0 NOT NULL,
-		  UNIQUE KEY id (id)
-		);";*/
+	if(get_option('cil_uninstall_db') == true)
+	{
+		global $wpdb;
 
-function myplugin_update_db_check() {
-    global $cil_db_version;
-    if (get_site_option('cil_db_version') != $cil_db_version) {
-        cil_install();
-    }
+		$listItemTableName = $wpdb->prefix . "cil_listItemInfo";
+	    $listTableName = $wpdb->prefix . "cil_listInfo";
+
+		$wpdb->query("DROP TABLE $listItemTableName");
+		$wpdb->query("DROP TABLE $listTableName");
+
+	}
+
+	delete_option('cil_uninstall_db');
+
 }
 
 
