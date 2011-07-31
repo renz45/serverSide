47b11e1f61188f8bcb0b656828eba5c9e8715dd4
Adam Rensel <adamrensel@gmail.com>
2011-07-31 12:42:38 -0400

    added license
    Signed-off-by: Adam Rensel <adamrensel@gmail.com>

--- cil/cil.php
+++ cil/cil.php
@@ -8,10 +8,25 @@
     Author URI: http://www.adamrensel.com
 */
 
+/*
+Copyright (C) 2011  Adam Rensel
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 //TODO ajax pagination for the user side
-//TODO option to remove the database
-//TODO finish nested list feature, when a list is nested, it gets output as a list item
-//TODO add an option (checkbox?) where a list can be set to edit only, no adding or deleting of items
 
 //////////////dev aids//////////////////
 include 'dev_aids/php_dump.php';
